7fea6b3cd254ea15db45852859f60b46132a3d7b
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-04-24 23:44:57 +0300

    Update FeedPage.jsx

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,4 +1,4 @@
-iimport { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabaseClient";
 
 export default function FeedPage() {
