ddfbd20b557605c04bc45d2dc638fa7cb0fa0bc5
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-04-26 16:08:42 +0300

    Add ResetPasswordPage route and update navbar logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 import FeedPage from "./pages/FeedPage";
 import VotePage from "./pages/VotePage";
 import NewPostPage from "./pages/NewPostPage";
@@ -13,7 +14,11 @@ import Navbar from "./components/Navbar";
 
 function AppShell() {
   const location = useLocation();
-  const hideNavbar = location.pathname === "/" || location.pathname === "/login";
+
+  const hideNavbar =
+    location.pathname === "/" ||
+    location.pathname === "/login" ||
+    location.pathname === "/reset";
 
   return (
     <>
@@ -22,12 +27,54 @@ function AppShell() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset" element={<ResetPasswordPage />} />
 
-        <Route path="/feed" element={<AuthGate><FeedPage /></AuthGate>} />
-        <Route path="/vote" element={<AuthGate><VotePage /></AuthGate>} />
-        <Route path="/new" element={<AuthGate><NewPostPage /></AuthGate>} />
-        <Route path="/groups" element={<AuthGate><GroupPage /></AuthGate>} />
-        <Route path="/profile" element={<AuthGate><ProfilePage /></AuthGate>} />
+        <Route
+          path="/feed"
+          element={
+            <AuthGate>
+              <FeedPage />
+            </AuthGate>
+          }
+        />
+
+        <Route
+          path="/vote"
+          element={
+            <AuthGate>
+              <VotePage />
+            </AuthGate>
+          }
+        />
+
+        <Route
+          path="/new"
+          element={
+            <AuthGate>
+              <NewPostPage />
+            </AuthGate>
+          }
+        />
+
+        <Route
+          path="/groups"
+          element={
+            <AuthGate>
+              <GroupPage />
+            </AuthGate>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <AuthGate>
+              <ProfilePage />
+            </AuthGate>
+          }
+        />
+
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </>
   );
