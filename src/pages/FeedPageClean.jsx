0fa56a8b9f7c27fcdff09285ace244aa562831e8
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-05-02 21:37:41 +0300

    chore: remove feed addiction layer from FeedPageClean

--- src/pages/FeedPageClean.jsx
+++ src/pages/FeedPageClean.jsx
@@ -4,7 +4,6 @@ import { mergeWithBots, botTicker, makeBotRepliesForPost } from "../lib/bots";
 import { haptic } from "../lib/effects";
 import { rankGodFeed, saveFeedSignal, whyForYou } from "../lib/godFeed";
 import { installFeedUltraProMotion } from "../lib/feedUltraProMotion";
-import { installFeedAddictionLayer } from "../lib/feedAddictionLayer";
 
 const FEED_BACKGROUNDS = [
   "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?auto=format&fit=crop&w=2600&q=95",
@@ -54,13 +53,9 @@ export default function FeedPageClean() {
   useEffect(() => {
     const startTimer = setTimeout(() => hideChrome(), 1900);
     let cleanupMotion = () => {};
-    let cleanupAddiction = () => {};
     const motionTimer = setTimeout(() => {
       cleanupMotion = installFeedUltraProMotion();
     }, 80);
-    const addictionTimer = setTimeout(() => {
-      cleanupAddiction = installFeedAddictionLayer();
-    }, 180);
     load();
     supabase.auth.getUser().then(({ data }) => setUser(data?.user || null));
     const tickerTimer = setInterval(() => setTicker(botTicker()), 5000);
@@ -76,10 +71,8 @@ export default function FeedPageClean() {
       clearTimeout(hintTimer);
       clearTimeout(startTimer);
       clearTimeout(motionTimer);
-      clearTimeout(addictionTimer);
       clearTimeout(chromeTimer.current);
       cleanupMotion?.();
-      cleanupAddiction?.();
       supabase.removeChannel(channel);
     };
   }, []);
