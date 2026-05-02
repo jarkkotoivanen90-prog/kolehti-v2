5aba0ae7b8bcb0da1df85d7f9a0738edfec6bfc1
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-05-02 16:17:57 +0300

    feat: enable ultra motion css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./next-level-ux.css";
+import "./feed-ultra-motion.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
