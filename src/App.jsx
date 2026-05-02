9585ef9d7255b9a10533a671a892933179fd953b
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-05-02 11:21:11 +0300

    hotfix: disable global chrome to restore app rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
-import { installGlobalHaptics, installReactiveUI } from "./lib/effects";
-import { startVersionCheck } from "./lib/versionCheck";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -17,60 +14,26 @@ import PotsPage from "./pages/PotsPage";
 import LeaderboardWarPage from "./pages/LeaderboardWarPage";
 
 import AuthGate from "./components/auth/AuthGate";
-import Navbar from "./components/Navbar";
-import BrandFX from "./components/BrandFX";
-import AppBottomNav from "./components/AppBottomNav";
 
 function AppShell() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const cleanHaptics = installGlobalHaptics();
-    const cleanReactive = installReactiveUI();
-    startVersionCheck();
-    return () => {
-      cleanHaptics();
-      cleanReactive();
-    };
-  }, []);
-
-  useEffect(() => {
-    try {
-      const params = new URLSearchParams(window.location.search);
-      const ref = params.get("ref");
-      if (ref) localStorage.setItem("kolehti_ref", ref);
-    } catch {}
-  }, [location.search]);
-
-  const authPage = location.pathname === "/login" || location.pathname === "/reset";
-  const hideNavbar = location.pathname === "/" || authPage;
-  const showGlobalBottomNav = !authPage;
-
   return (
-    <>
-      <BrandFX />
-      {!hideNavbar && <Navbar />}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/reset" element={<ResetPasswordPage />} />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/reset" element={<ResetPasswordPage />} />
+      <Route path="/feed" element={<AuthGate><FeedPage /></AuthGate>} />
+      <Route path="/vote" element={<AuthGate><VotePage /></AuthGate>} />
+      <Route path="/new" element={<AuthGate><NewPostPage /></AuthGate>} />
+      <Route path="/groups" element={<AuthGate><GroupPage /></AuthGate>} />
+      <Route path="/profile" element={<AuthGate><ProfilePage /></AuthGate>} />
+      <Route path="/growth" element={<AuthGate><GrowthPage /></AuthGate>} />
+      <Route path="/leaderboard" element={<AuthGate><LeaderboardPage /></AuthGate>} />
+      <Route path="/pots" element={<AuthGate><PotsPage /></AuthGate>} />
+      <Route path="/war" element={<AuthGate><LeaderboardWarPage /></AuthGate>} />
 
-        <Route path="/feed" element={<AuthGate><FeedPage /></AuthGate>} />
-        <Route path="/vote" element={<AuthGate><VotePage /></AuthGate>} />
-        <Route path="/new" element={<AuthGate><NewPostPage /></AuthGate>} />
-        <Route path="/groups" element={<AuthGate><GroupPage /></AuthGate>} />
-        <Route path="/profile" element={<AuthGate><ProfilePage /></AuthGate>} />
-        <Route path="/growth" element={<AuthGate><GrowthPage /></AuthGate>} />
-        <Route path="/leaderboard" element={<AuthGate><LeaderboardPage /></AuthGate>} />
-        <Route path="/pots" element={<AuthGate><PotsPage /></AuthGate>} />
-        <Route path="/war" element={<AuthGate><LeaderboardWarPage /></AuthGate>} />
-
-        <Route path="*" element={<HomePage />} />
-      </Routes>
-
-      {showGlobalBottomNav && <AppBottomNav floating gesture />}
-    </>
+      <Route path="*" element={<HomePage />} />
+    </Routes>
   );
 }
 
