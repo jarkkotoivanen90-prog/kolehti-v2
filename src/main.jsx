409fa50418bf41a06eb9ba1e37a614f1aaec9daf
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-05-02 11:17:02 +0300

    fix: remove risky pots polish and cache cleanup from app startup

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./next-level-ux.css";
-import "./pots-polish.css";
-
-// 🔥 HARD CACHE BUST + CLEANUP
-(async () => {
-  try {
-    if ("caches" in window) {
-      const keys = await caches.keys();
-      await Promise.all(keys.map((k) => caches.delete(k)));
-    }
-
-    if ("serviceWorker" in navigator) {
-      const regs = await navigator.serviceWorker.getRegistrations();
-      for (const reg of regs) {
-        await reg.unregister();
-      }
-    }
-  } catch (e) {
-    console.warn("cache cleanup failed", e);
-  }
-})();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
