203458cea68e9b1e8fdf92305a9a035b5de5914d
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-05-05 22:11:41 +0300

    Update xpEvents.js

--- src/lib/xpEvents.js
+++ src/lib/xpEvents.js
@@ -1,14 +1,25 @@
-const listeners = [];
+// src/lib/xpEvents.js
+
+const globalKey = "__xp_event_bus__";
+
+if (!window[globalKey]) {
+  window[globalKey] = {
+    listeners: [],
+  };
+}
+
+const bus = window[globalKey];
 
 export function emitXPEvent(event) {
   console.log("EMIT XP", event);
-  listeners.forEach((fn) => fn(event));
+  bus.listeners.forEach((fn) => fn(event));
 }
 
 export function onXPEvent(fn) {
-  listeners.push(fn);
+  bus.listeners.push(fn);
+
   return () => {
-    const i = listeners.indexOf(fn);
-    if (i > -1) listeners.splice(i, 1);
+    const i = bus.listeners.indexOf(fn);
+    if (i > -1) bus.listeners.splice(i, 1);
   };
 }
