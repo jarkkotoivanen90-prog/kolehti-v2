1afb4940b4707ec8db4a0ff0882735714d1bb29a
jarkkotoivanen90-prog <jarkko.toivanen90@gmail.com>
2026-05-05 21:58:25 +0300

    Update xpEvents.js

--- src/lib/xpEvents.js
+++ src/lib/xpEvents.js
@@ -1,16 +1,14 @@
-const listeners = new Set();
+const listeners = [];
 
-export function emitXPEvent(data) {
-  listeners.forEach((fn) => {
-    try {
-      fn(data);
-    } catch (err) {
-      console.error("XP listener error:", err);
-    }
-  });
+export function emitXPEvent(event) {
+  console.log("EMIT XP", event);
+  listeners.forEach((fn) => fn(event));
 }
 
 export function onXPEvent(fn) {
-  listeners.add(fn);
-  return () => listeners.delete(fn);
+  listeners.push(fn);
+  return () => {
+    const i = listeners.indexOf(fn);
+    if (i > -1) listeners.splice(i, 1);
+  };
 }
